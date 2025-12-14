9249be228bc2b7fdccf80a233bb953c4b4761508
Bakberdi <bakberdy.ye@gmail.com>
2025-12-14 09:02:43 +0500

    Initial Commit

---
+++ ayaq/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ayaq
+//
+//  Created by Bakberdi Esentai on 14.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
